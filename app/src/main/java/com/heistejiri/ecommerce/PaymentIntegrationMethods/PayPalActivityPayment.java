19e6d59e95368a3a7adbf7ce6ae1d101eef7ffb7
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-26 18:04:11 +0100

    Create PayPalActivityPayment.java

---
+++ app/src/main/java/com/heistejiri/ecommerce/PaymentIntegrationMethods/PayPalActivityPayment.java
@@ -0,0 +1,16 @@
+package com.heistejiri.ecommerce.PaymentIntegrationMethods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heistejiri.ecommerce.R;
+
+public class PayPalActivityPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_pal_payment);
+    }
+}
