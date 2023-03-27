dcc6047f60f1a8bb2ff33f8b908c73c64aa835f0
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-27 18:31:43 +0100

    Update CategorieListViewHolder.java

--- app/src/main/java/com/heistejiri/ecommerce/Adapters/CategorieListViewHolder.java
+++ app/src/main/java/com/heistejiri/ecommerce/Adapters/CategorieListViewHolder.java
@@ -1,4 +1,36 @@
 package com.heistejiri.ecommerce.Adapters;
 
-public class CategorieListViewHolder {
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.heistejiri.ecommerce.Fragments.ProductsList;
+import com.heistejiri.ecommerce.Activities.MainActivity;
+import com.heistejiri.ecommerce.R;
+
+
+/**
+ * Created by Android
+ */
+public class CategorieListViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView image;
+    TextView catName;
+    CardView cardView;
+
+    public CategorieListViewHolder(final Context context, View itemView) {
+        super(itemView);
+        image = (ImageView) itemView.findViewById(R.id.categoryIcon);
+        catName = (TextView) itemView.findViewById(R.id.categoryName);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProductsList.categoryPosition = getAdapterPosition();
+                ((MainActivity) context).loadFragment(new ProductsList(), true);
+            }
+        });
+    }
 }
