90d06dc292725b9f5354452843d5f1014adf043b
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-27 18:33:46 +0100

    Update MyOrdersViewHolder.java

--- app/src/main/java/com/heistejiri/ecommerce/Adapters/MyOrdersViewHolder.java
+++ app/src/main/java/com/heistejiri/ecommerce/Adapters/MyOrdersViewHolder.java
@@ -1,4 +1,31 @@
 package com.heistejiri.ecommerce.Adapters;
 
-public class MyOrdersViewHolder {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.heistejiri.ecommerce.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * Created by Android
+ */
+public class MyOrdersViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.orderedProductsRecyclerView)
+    RecyclerView orderedProductsRecyclerView;
+    @BindView(R.id.viewOrderDetails)
+    TextView viewOrderDetails;
+    @BindView(R.id.date)
+    TextView date;
+
+
+    public MyOrdersViewHolder(final Context context, View itemView) {
+        super(itemView);
+        ButterKnife.bind(this,itemView);
+    }
 }
