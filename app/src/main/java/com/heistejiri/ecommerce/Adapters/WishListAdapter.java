5f6088e2a7c330b7da19da9357f142c0c1e8904a
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-27 18:33:40 +0100

    Update WishListAdapter.java

--- app/src/main/java/com/heistejiri/ecommerce/Adapters/WishListAdapter.java
+++ app/src/main/java/com/heistejiri/ecommerce/Adapters/WishListAdapter.java
@@ -1,4 +1,139 @@
 package com.heistejiri.ecommerce.Adapters;
 
-public class WishListAdapter {
+import android.content.Context;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.heistejiri.ecommerce.Config;
+import com.heistejiri.ecommerce.Fragments.MyWishList;
+import com.heistejiri.ecommerce.Fragments.ProductDetail;
+import com.heistejiri.ecommerce.MVP.AddToWishlistResponse;
+import com.heistejiri.ecommerce.MVP.Product;
+import com.heistejiri.ecommerce.Activities.MainActivity;
+import com.heistejiri.ecommerce.R;
+import com.heistejiri.ecommerce.Retrofit.Api;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+
+/**
+ * Created by Android
+ */
+public class WishListAdapter extends RecyclerView.Adapter<HomeProductsViewHolder> {
+    Context context;
+    List<Product> productList;
+
+    public WishListAdapter(Context context, List<Product> productList) {
+        this.context = context;
+        this.productList = productList;
+    }
+
+    @Override
+    public HomeProductsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.wish_list_items, null);
+        HomeProductsViewHolder homeProductsViewHolder = new HomeProductsViewHolder(context, view, productList);
+        return homeProductsViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final HomeProductsViewHolder holder, final int position) {
+
+
+        holder.cardView.setVisibility(View.GONE);
+        holder.cardView1.setVisibility(View.VISIBLE);
+        holder.productName1.setText(productList.get(position).getProductName());
+        holder.price1.setText(MainActivity.currency + " " + productList.get(position).getSellprice());
+        try {
+            Picasso.with(context)
+                    .load(productList.get(position).getImages().get(0))
+                    .resize(Integer.parseInt(context.getResources().getString(R.string.targetProductImageWidth1)),Integer.parseInt(context.getResources().getString(R.string.targetProductImageHeight)))
+                    .placeholder(R.drawable.defaultimage)
+                    .into(holder.image1);
+        } catch (Exception e) {
+        }
+        try {
+            double discountPercentage = Integer.parseInt(productList.get(position).getMrpprice()) - Integer.parseInt(productList.get(position).getSellprice());
+            Log.d("percentage", discountPercentage + "");
+            discountPercentage = (discountPercentage / Integer.parseInt(productList.get(position).getMrpprice())) * 100;
+            if ((int) Math.round(discountPercentage) > 0) {
+                holder.discountPercentage1.setText(((int) Math.round(discountPercentage) + "% Off"));
+            }
+            holder.actualPrice1.setText(MainActivity.currency + " " + productList.get(position).getMrpprice());
+            holder.actualPrice1.setPaintFlags(holder.actualPrice1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        }catch (Exception e){}
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProductDetail.productList.clear();
+                ProductDetail.productList.addAll(productList);
+                ProductDetail productDetail = new ProductDetail();
+                Bundle bundle = new Bundle();
+                bundle.putInt("position", position);
+                productDetail.setArguments(bundle);
+                ((MainActivity) context).loadFragment(productDetail, true);
+            }
+        });
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addToWishList(position);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    private void addToWishList(final int position) {
+        final SweetAlertDialog pDialog = new SweetAlertDialog(context, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(context.getResources().getColor(R.color.colorPrimary));
+        pDialog.setTitleText("Loading");
+        pDialog.setCancelable(false);
+        pDialog.show();
+        Api.getClient().addToWishList(productList.get(position).getProductId(),
+                MainActivity.userId,
+                new Callback<AddToWishlistResponse>() {
+                    @Override
+                    public void success(AddToWishlistResponse addToWishlistResponse, Response response) {
+                        pDialog.dismiss();
+
+                        Log.d("addToWishListResponse", addToWishlistResponse.getSuccess() + "");
+                        if (addToWishlistResponse.getSuccess().equalsIgnoreCase("true")) {
+                            ((MainActivity) context).loadFragment(new MyWishList(), false);
+                            Config.showCustomAlertDialog(context,
+                                    "Your wishlist status",
+                                    addToWishlistResponse.getMessage(),
+                                    SweetAlertDialog.SUCCESS_TYPE);
+                        }else {
+                            Config.showCustomAlertDialog(context,
+                                    "Your wishlist status",
+                                    addToWishlistResponse.getMessage(),
+                                    SweetAlertDialog.NORMAL_TYPE);
+                        }
+
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        pDialog.dismiss();
+
+                        Log.e("error", error.toString());
+                    }
+                });
+    }
 }
