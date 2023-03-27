4a662c3fd53e6427381ad93b5814d0b8bde242c6
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-27 18:33:33 +0100

    Update DotViewHolder.java

--- app/src/main/java/com/heistejiri/ecommerce/Adapters/DotViewHolder.java
+++ app/src/main/java/com/heistejiri/ecommerce/Adapters/DotViewHolder.java
@@ -1,4 +1,23 @@
 package com.heistejiri.ecommerce.Adapters;
 
-public class DotViewHolder {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.heistejiri.ecommerce.R;
+
+
+/**
+ * Created by Android
+ */
+public class DotViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView dotImageView;
+
+    public DotViewHolder(final Context context, View itemView) {
+        super(itemView);
+        dotImageView = (ImageView) itemView.findViewById(R.id.dotImageView);
+
+    }
 }
