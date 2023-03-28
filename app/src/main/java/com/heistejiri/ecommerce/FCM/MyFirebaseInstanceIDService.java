74a5238507aaa3a0eaf5ef528ae25c937f5f6565
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-28 22:15:29 +0100

    Update MyFirebaseInstanceIDService.java

--- app/src/main/java/com/heistejiri/ecommerce/FCM/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/heistejiri/ecommerce/FCM/MyFirebaseInstanceIDService.java
@@ -1,4 +1,62 @@
 package com.heistejiri.ecommerce.FCM;
 
-public class MyFirebaseInstanceIDService {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.heistejiri.ecommerce.Config;
+import com.heistejiri.ecommerce.MVP.RegistrationResponse;
+import com.heistejiri.ecommerce.Retrofit.Api;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = MyFirebaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        // Saving reg id to shared preferences
+        storeRegIdInPref(refreshedToken);
+
+        // sending reg id to your server
+        sendRegistrationToServer(refreshedToken);
+
+        // Notify UI that registration has completed, so the progress indicator can be hidden.
+        Intent registrationComplete = new Intent(Config.REGISTRATION_COMPLETE);
+        registrationComplete.putExtra("token", refreshedToken);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+
+    private void sendRegistrationToServer(final String token) {
+        // sending gcm token to server
+        Log.e(TAG, "sendRegistrationToServer: " + token);
+        Api.getClient().sendAccessToken(token, new Callback<RegistrationResponse>() {
+            @Override
+            public void success(RegistrationResponse registrationResponse, Response response) {
+                Log.d("registrationResponse",registrationResponse.getSuccess());
+
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e("error", error.toString());
+            }
+        });
+    }
+
+    private void storeRegIdInPref(String token) {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("regId", token);
+        editor.commit();
+    }
 }
