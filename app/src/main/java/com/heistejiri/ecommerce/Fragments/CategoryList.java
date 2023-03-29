c5404536a8df71ace85f5d92f185c980a9004eeb
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-29 09:07:39 +0100

    Update CategoryList.java

--- app/src/main/java/com/heistejiri/ecommerce/Fragments/CategoryList.java
+++ app/src/main/java/com/heistejiri/ecommerce/Fragments/CategoryList.java
@@ -1,66 +1,57 @@
 package com.heistejiri.ecommerce.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.heistejiri.ecommerce.Adapters.CategoryListAdapter;
+import com.heistejiri.ecommerce.Config;
+import com.heistejiri.ecommerce.Activities.MainActivity;
 import com.heistejiri.ecommerce.R;
+import com.heistejiri.ecommerce.Activities.SplashScreen;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CategoryList#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CategoryList extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CategoryList() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CategoryList.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CategoryList newInstance(String param1, String param2) {
-        CategoryList fragment = new CategoryList();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    View view;
+    @BindView(R.id.categoryRecyclerView)
+    RecyclerView categoryRecyclerView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_list, container, false);
+        view = inflater.inflate(R.layout.fragment_category_list, container, false);
+        ButterKnife.bind(this, view);
+        setCategoryData();
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        ((MainActivity) getActivity()).lockUnlockDrawer(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        MainActivity.search.setVisibility(View.VISIBLE);
+        MainActivity.title.setText("Categories");
+        Config.getCartList(getActivity(),true);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        MainActivity.search.setVisibility(View.GONE);
+    }
+
+    private void setCategoryData() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        categoryRecyclerView.setLayoutManager(gridLayoutManager);
+        CategoryListAdapter categoryListAdapter = new CategoryListAdapter(getActivity(), SplashScreen.categoryListResponseData);
+        categoryRecyclerView.setAdapter(categoryListAdapter);
     }
 }
