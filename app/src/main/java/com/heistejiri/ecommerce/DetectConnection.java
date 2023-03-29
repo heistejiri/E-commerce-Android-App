907b75d3831f3f155788b38719d98c65d5a7ff69
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-29 09:07:29 +0100

    Update DetectConnection.java

--- app/src/main/java/com/heistejiri/ecommerce/DetectConnection.java
+++ app/src/main/java/com/heistejiri/ecommerce/DetectConnection.java
@@ -1,4 +1,21 @@
 package com.heistejiri.ecommerce;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+
 public class DetectConnection {
+    public static boolean checkInternetConnection(Context context) {
+        // detect internet connection
+        ConnectivityManager con_manager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (con_manager.getActiveNetworkInfo() != null
+                && con_manager.getActiveNetworkInfo().isAvailable()
+                && con_manager.getActiveNetworkInfo().isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
