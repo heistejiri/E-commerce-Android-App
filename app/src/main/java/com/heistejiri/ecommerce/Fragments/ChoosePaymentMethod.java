65c65222f257da997e5b7de3ac8173dfec5b0217
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-29 09:07:47 +0100

    Update ChoosePaymentMethod.java

--- app/src/main/java/com/heistejiri/ecommerce/Fragments/ChoosePaymentMethod.java
+++ app/src/main/java/com/heistejiri/ecommerce/Fragments/ChoosePaymentMethod.java
@@ -1,66 +1,254 @@
 package com.heistejiri.ecommerce.Fragments;
 
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import com.heistejiri.ecommerce.Activities.MainActivity;
+import com.heistejiri.ecommerce.Config;
+import com.heistejiri.ecommerce.MVP.UserProfileResponse;
+//import com.heistejiri.ecommerce.PaymentIntegrationMethods.PayPalActivityPayment;
+//import com.heistejiri.ecommerce.PaymentIntegrationMethods.StripePaymentIntegration;
+import com.heistejiri.ecommerce.PaymentIntegrationMethods.RazorPayIntegration;
 import com.heistejiri.ecommerce.R;
+import com.heistejiri.ecommerce.Retrofit.Api;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChoosePaymentMethod#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ChoosePaymentMethod extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ChoosePaymentMethod() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChoosePaymentMethod.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChoosePaymentMethod newInstance(String param1, String param2) {
-        ChoosePaymentMethod fragment = new ChoosePaymentMethod();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    View view;
+    @BindView(R.id.addNewAddressLayout)
+    LinearLayout addNewAddressLayout;
+    @BindView(R.id.addressCheckBox)
+    CheckBox addressCheckBox;
+    @BindView(R.id.addNewAddress)
+    TextView addNewAddress;
+    @BindView(R.id.fillAddress)
+    TextView fillAddress;
+    @BindView(R.id.paymentMethodsGroup)
+    RadioGroup paymentMethodsGroup;
+    @BindView(R.id.makePayment)
+    Button makePayment;
+    String paymentMethod;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.choosePaymentLayout)
+    LinearLayout choosePaymentLayout;
+    @BindViews({R.id.fullNameEdt, R.id.mobEditText, R.id.cityEditText, R.id.areaEditText, R.id.buildingEditText, R.id.pincodeEditText, R.id.stateEditText, R.id.landmarkEditText,})
+    List<EditText> editTexts;
+    public static String address, mobileNo,userEmail;
+    Intent intent;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_payment_method, container, false);
+
+        int layout = R.layout.fragment_choose_payment_method;
+        view = inflater.inflate(layout, container, false);
+        ButterKnife.bind(this, view);
+        MainActivity.title.setText("Choose Payment Method");
+        MainActivity.cart.setVisibility(View.GONE);
+        MainActivity.cartCount.setVisibility(View.GONE);
+        getUserProfileData();
+        addressCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    addNewAddressLayout.setVisibility(View.GONE);
+                    addNewAddress.setText("Add New Address");
+
+                }
+            }
+        });
+        choosePaymentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideKeyboard(view);
+
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        MainActivity.cart.setVisibility(View.VISIBLE);
+        MainActivity.cartCount.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick({R.id.addNewAddress, R.id.makePayment, R.id.fillAddress})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.addNewAddress:
+                addNewAddressLayout.setVisibility(View.VISIBLE);
+                addressCheckBox.setChecked(false);
+                addNewAddress.setText("Use This Address");
+                break;
+            case R.id.makePayment:
+                if (!addressCheckBox.isChecked()) {
+                    if (addNewAddressLayout.getVisibility() == View.VISIBLE) {
+                        if (validate(editTexts.get(0))
+                                && validate(editTexts.get(1))
+                                && validate(editTexts.get(2))
+                                && validate(editTexts.get(3))
+                                && validate(editTexts.get(4))
+                                && validate(editTexts.get(5))
+                                && validate(editTexts.get(6))) {
+                            String s = "";
+                            if (editTexts.get(6).getText().toString().trim().length() > 0) {
+                                s = ", " + editTexts.get(6).getText().toString().trim();
+                            }
+                            address = editTexts.get(0).getText().toString().trim()
+                                    + ", "
+                                    + editTexts.get(4).getText().toString().trim()
+                                    + s
+                                    + ", " + editTexts.get(3).getText().toString().trim()
+                                    + ", " + editTexts.get(2).getText().toString().trim()
+                                    + ", " + editTexts.get(6).getText().toString().trim()
+                                    + ", " + editTexts.get(5).getText().toString().trim()
+                                    + "\n" + editTexts.get(1).getText().toString().trim();
+                            mobileNo = editTexts.get(1).getText().toString().trim();
+                            moveNext();
+                        }
+                    } else {
+                        Config.showCustomAlertDialog(getActivity(),
+                                "Please choose your saved address or add new to make payment",
+                                "",
+                                SweetAlertDialog.ERROR_TYPE);
+                    }
+                } else {
+                    moveNext();
+                }
+
+                break;
+            case R.id.fillAddress:
+                ((MainActivity) getActivity()).loadFragment(new MyProfile(), true);
+                break;
+        }
+
+    }
+    protected void hideKeyboard(View view)
+    {
+        InputMethodManager in = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        in.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+    private void moveNext() {
+        switch (paymentMethodsGroup.getCheckedRadioButtonId()) {
+            //case R.id.paypal:
+            //  paymentMethod = "paypal";
+            //intent = new Intent(getActivity(), PayPalActivityPayment.class);
+            //startActivity(intent);
+            //break;
+            case R.id.cod:
+                paymentMethod = "cod";
+                Config.addOrder(getActivity(),
+                        "COD",
+                        "COD");
+                break;
+            //case R.id.stripe:
+            //  paymentMethod = "stripe";
+            //intent = new Intent(getActivity(), StripePaymentIntegration.class);
+            //startActivity(intent);
+            //break;
+
+            case R.id.razorPay:
+                paymentMethod = "razorPay";
+                intent = new Intent(getActivity(), RazorPayIntegration.class);
+                startActivity(intent);
+                break;
+            default:
+                paymentMethod = "";
+                Config.showCustomAlertDialog(getActivity(),
+                        "Payment Method",
+                        "Select your payment method to make payment",
+                        SweetAlertDialog.NORMAL_TYPE);
+                break;
+
+
+        }
+
+        Log.d("paymentMethod", paymentMethod);
+    }
+
+    private boolean validate(EditText editText) {
+        if (editText.getText().toString().trim().length() > 0) {
+            return true;
+        }
+        editText.setError("Please Fill This");
+        editText.requestFocus();
+        return false;
+    }
+
+    public void getUserProfileData() {
+        progressBar.setVisibility(View.VISIBLE);
+        Api.getClient().getUserProfile(
+                MainActivity.userId, new Callback<UserProfileResponse>() {
+                    @Override
+                    public void success(UserProfileResponse userProfileResponse, Response response) {
+                        progressBar.setVisibility(View.GONE);
+                        userEmail=userProfileResponse.getEmail();
+                        String s = "";
+                        if (!userProfileResponse.getLandmark().equalsIgnoreCase("")) {
+                            s = ", " + userProfileResponse.getLandmark();
+                        }
+                        if (userProfileResponse.getFlat().equalsIgnoreCase("")) {
+                            addressCheckBox.setChecked(false);
+                            addressCheckBox.setVisibility(View.GONE);
+                            fillAddress.setVisibility(View.VISIBLE);
+                        } else {
+                            address = userProfileResponse.getName()
+                                    + ", "
+                                    + userProfileResponse.getFlat()
+                                    + s
+                                    + ", " + userProfileResponse.getLocality()
+                                    + ", " + userProfileResponse.getCity()
+                                    + ", " + userProfileResponse.getState()
+                                    + ", " + userProfileResponse.getPincode()
+                                    + "\n" + userProfileResponse.getMobile();
+                            addressCheckBox.setText(address);
+                            mobileNo = userProfileResponse.getMobile();
+                        }
+
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        progressBar.setVisibility(View.GONE);
+
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        ((MainActivity) getActivity()).lockUnlockDrawer(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
     }
 }
+
