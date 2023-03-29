68b680449992b165060bd225e63d9668f7c97def
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-29 09:07:35 +0100

    Update AppInfo.java

--- app/src/main/java/com/heistejiri/ecommerce/Fragments/AppInfo.java
+++ app/src/main/java/com/heistejiri/ecommerce/Fragments/AppInfo.java
@@ -1,66 +1,46 @@
 package com.heistejiri.ecommerce.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.heistejiri.ecommerce.Config;
+import com.heistejiri.ecommerce.Activities.MainActivity;
 import com.heistejiri.ecommerce.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AppInfo#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AppInfo extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AppInfo() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AppInfo.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AppInfo newInstance(String param1, String param2) {
-        AppInfo fragment = new AppInfo();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app_info, container, false);
+        view = inflater.inflate(R.layout.fragment_more, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick({R.id.termsLayout})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.termsLayout:
+                ((MainActivity) getActivity()).loadFragment(new TermsAndConditions(), true);
+                break;
+
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        ((MainActivity) getActivity()).lockUnlockDrawer(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        MainActivity.title.setText("App Info");
+        Config.getCartList(getActivity(), true);
     }
 }
