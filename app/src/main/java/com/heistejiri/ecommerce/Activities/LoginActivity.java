baf75bf621a5d99b392117250ffaa86c217ec92c
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-30 20:17:41 +0100

    Update LoginActivity.java

--- app/src/main/java/com/heistejiri/ecommerce/Activities/LoginActivity.java
+++ app/src/main/java/com/heistejiri/ecommerce/Activities/LoginActivity.java
@@ -1,16 +1,133 @@
 package com.heistejiri.ecommerce.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.heistejiri.ecommerce.Common;
+import com.heistejiri.ecommerce.Config;
+import com.heistejiri.ecommerce.MVP.SignUpResponse;
+import com.heistejiri.ecommerce.Retrofit.Api;
 import com.heistejiri.ecommerce.R;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    @BindViews({R.id.email, R.id.password})
+    List<EditText> editTexts;
+    @BindView(R.id.loginLinearLayout)
+    LinearLayout loginLinearLayout;
+    @BindView(R.id.appIcon)
+    ImageView appIcon;
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        loginLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideKeyboard(view);
+
+            }
+        });
+
+    }
+    protected void hideKeyboard(View view)
+    {
+        InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        in.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+    @OnClick({R.id.txtSignUp, R.id.txtForgotPassword, R.id.skipLoginLayout, R.id.signIn})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.txtSignUp:
+                Config.moveTo(LoginActivity.this, SignUpActivity.class);
+                break;
+            case R.id.txtForgotPassword:
+                Config.moveTo(LoginActivity.this, ForgotPassword.class);
+                break;
+            case R.id.skipLoginLayout:
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("from", "skip");
+                startActivity(intent);
+                finishAffinity();
+                break;
+
+            case R.id.signIn:
+                if (Config.validateEmail(editTexts.get(0),LoginActivity.this) && validatePassword(editTexts.get(1))) {
+                    login();
+                }
+                break;
+        }
+    }
+
+    private boolean validatePassword(EditText editText) {
+        if (editText.getText().toString().trim().length() > 5) {
+            return true;
+        } else if (editText.getText().toString().trim().length() > 0) {
+            editText.setError("Password must be of 6 characters");
+            editText.requestFocus();
+            return false;
+        }
+        editText.setError("Please Fill This");
+        editText.requestFocus();
+        return false;
+    }
+
+    private void login() {
+        final SweetAlertDialog pDialog = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(getResources().getColor(R.color.colorPrimary));
+        pDialog.setTitleText("Loading");
+        pDialog.setCancelable(false);
+        pDialog.show();
+        // sending gcm token to server
+        Api.getClient().login(editTexts.get(0).getText().toString().trim(),
+                editTexts.get(1).getText().toString().trim(),
+                "email",
+                new Callback<SignUpResponse>() {
+                    @Override
+                    public void success(SignUpResponse signUpResponse, Response response) {
+                        pDialog.dismiss();
+                        Log.d("signUpResponse", signUpResponse.getUserid() + "");
+                        Toast.makeText(LoginActivity.this, signUpResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                        if (signUpResponse.getSuccess().equalsIgnoreCase("true")) {
+                            Common.saveUserData(LoginActivity.this, "email", editTexts.get(1).getText().toString());
+                            Common.saveUserData(LoginActivity.this, "userId", signUpResponse.getUserid() + "");
+                            Config.moveTo(LoginActivity.this, MainActivity.class);
+                            finishAffinity();
+                        } else if (signUpResponse.getSuccess().equalsIgnoreCase("notactive")) {
+                            Config.moveTo(LoginActivity.this, AccountVerification.class);
+                        }
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        pDialog.dismiss();
+
+                        Log.e("error", error.toString());
+                    }
+                });
     }
 }
